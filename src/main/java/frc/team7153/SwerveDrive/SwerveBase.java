5306781ff075e44306c1dbbd4ca3414123cf60de
braykoff <braykoff2007@gmail.com>
2022-12-24 21:56:32 -0500

    Added SwerveDrive

---
+++ src/main/java/frc/team7153/SwerveDrive/SwerveBase.java
@@ -0,0 +1,148 @@
+package frc.team7153.SwerveDrive;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveBase extends SubsystemBase {
+    // Wheels
+    private SwerveWheel fl;
+    private SwerveWheel fr;
+    private SwerveWheel rl;
+    private SwerveWheel rr;
+    
+    // Kinematics
+    private SwerveDriveKinematics kinematics;
+
+    // State
+    private boolean periodicRunning = true;
+    
+    /**
+     * Creates a new SwerveBase, with four SwerveWheels.
+     * @param frontLeft
+     * @param frontRight
+     * @param rearLeft
+     * @param rearRight
+     */
+    public SwerveBase(SwerveWheel frontLeft, SwerveWheel frontRight, SwerveWheel rearLeft, SwerveWheel rearRight) {
+        fl = frontLeft;
+        fr = frontRight;
+        rl = rearLeft;
+        rr = rearRight;
+
+        kinematics = new SwerveDriveKinematics(fl.pos, fr.pos, rl.pos, rr.pos);
+    }
+
+    /**
+     * Drives the robot
+     * @param y forward/backward speed (meters per second)
+     * @param x left/right speed (meters per second)
+     * @param r rotation speed (degrees per second)
+     */
+    public void drive(double y, double x, double r) {
+        ChassisSpeeds speed = new ChassisSpeeds(y, x, -SwerveMathUtils.degreesToRadians(r));
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speed);
+
+        fl.set(states[0]);
+        fr.set(states[1]);
+        rl.set(states[2]);
+        rr.set(states[3]);
+        periodicRunning = true;
+    }
+
+    /**
+     * Drives the robot, with field-oriented drive
+     * @param y forward/backward speed (meters per second)
+     * @param x left/right speed (meters per second)
+     * @param r rotation speed (degrees per second)
+     * @param deg current angle of the robot (degrees)
+     */
+    public void driveFieldOriented(double y, double x, double r, double deg) {
+        ChassisSpeeds speed = ChassisSpeeds.fromFieldRelativeSpeeds(
+            y, x, 
+            -SwerveMathUtils.degreesToRadians(r), 
+            Rotation2d.fromDegrees(deg)
+        );
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speed);
+
+        fl.set(states[0]);
+        fr.set(states[1]);
+        rl.set(states[2]);
+        rr.set(states[3]);
+        periodicRunning = true;
+    }
+
+    /**
+     * Orbits the robot around a point
+     * @param rotation The speed, in degrees per second
+     * @param centerX The X position of the center of rotation, relative to the robot's base (meters)
+     * @param centerY The Y position of the center of rotation, relative to the robot's base (meters)
+     */
+    public void orbit(double rotation, double centerX, double centerY) {
+        ChassisSpeeds speed = new ChassisSpeeds(0.0, 0.0, -SwerveMathUtils.degreesToRadians(rotation));
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speed, new Translation2d(centerX, centerY));
+
+        fl.set(states[0]);
+        fr.set(states[1]);
+        rl.set(states[2]);
+        rr.set(states[3]);
+        periodicRunning = true;
+    }
+
+    /**
+     * Drives the robot like its a tank drive base
+     * @param left The speed of the left side of the robot (meters/second)
+     * @param right The speed of the right side of the robot (meters/second)
+     */
+    public void tankDrive(double left, double right) {
+        fl.set(0.0, left); // TODO: convert
+        rl.set(0.0, left);
+        fr.set(0.0, right);
+        rr.set(0.0, right);
+
+        periodicRunning = true;
+    }
+
+    /**
+     * Stops the robot
+     * @param reset Whether the wheels should return to a forward position
+     */
+    public void stop(boolean reset) {
+        if (reset) {
+            fl.set(0.0, 0.0);
+            fr.set(0.0, 0.0);
+            fr.set(0.0, 0.0);
+            rr.set(0.0, 0.0);
+        } else {
+            fl.setSpeed(0.0);
+            fr.setSpeed(0.0);
+            fr.setSpeed(0.0);
+            rr.setSpeed(0.0);
+        }
+    }
+
+    /**
+     * Stops the periodic methods.
+     * Periodic methods will resume as soon as one of the drive methods are called again.
+     * @param reset Whether the motor speeds should be set to 0 (if false, the robot may keep moving)
+     */
+    public void stopPeriodic(boolean reset) {
+        if (reset) { stop(false); periodic(); }
+        periodicRunning = false;
+    }
+
+    // Periodic
+    @Override
+    public void periodic() {
+        if (!DriverStation.isDisabled() && periodicRunning) {
+            fl.periodic();
+            fr.periodic();
+            rl.periodic();
+            rr.periodic();
+        }
+    }
+}
---
+++ src/main/java/frc/team7153/SwerveDrive/SwerveMathUtils.java
@@ -0,0 +1,48 @@
+package frc.team7153.SwerveDrive;
+
+public class SwerveMathUtils {
+    /**
+     * Takes an angle and normalizes it to a range of 0 - 360
+     * @param angle
+     * @return The angle, normalized
+     */
+    public static double normalizeAngle(double angle) {
+        if (angle > 360) {
+            return angle - (Math.floor(angle/360)*360);
+        } else if (angle < 0) {
+            return angle + (Math.ceil(Math.abs(angle/360))*360);
+        } else { return angle; }
+    }
+
+    /**
+     * Clamps a speed to the specified range, assuming the lower bound is the opposite of the upper bound
+     * @param value The input value
+     * @param clamp The upper bound, and opposite of the lower bound
+     * @return The clamped value
+     */
+    public static double symmetricClamp(double value, double clamp) {
+        if (clamp < 0) { clamp = -clamp;}
+        return Math.min(Math.max(value, -clamp), clamp);
+    }
+
+    /**
+     * Convert RPMs to Falcon500's encoder velocity
+     * @param rpm
+     * @return Falcon500's encoder velocity
+     */
+    public static double rpmToFalcon500Velocity(double rpm) { return rpm * 2048 * 0.1; }
+
+    /**
+     * Convert the Falcon500's velocity to RPMs
+     * @param velocity
+     * @return RPMs
+     */
+    public static double Falcon500VelocityToRPM(double velocity) { return velocity / 2048 / 0.1; }
+
+    /**
+     * Convert degrees to radians
+     * @param angles
+     * @return radians
+     */
+    public static double degreesToRadians(double angles) { return angles * (Math.PI/180); }
+}
